6254c99d1fd932cf7663a76f71c50858f8226645
vamsi <vamsi@mundhadugu.org>
2014-07-10 20:06:32 +0530

    commit for store controller and views---- shop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def image_load_tag(image_url)
+    image = image_tag(image_url)
+    #return image if image.present?
+    return image_tag("/images/rails.png")
+  end
+  
+  
 end
