49bc8bf84af21caaf2057395b95bd0b4debf5619
vamsi <vamsi@mundhadugu.org>
2014-07-10 16:55:35 +0530

    shop app initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shop::Application.config.secret_key_base = '03e708b5c4ba75dc2cbef51f8db161dcc15ac70ea5b02c5dee7c46ba75daa672b6db57b3a04d85975e9a8f2aed6c6e4fbff942598147ff3d6a018a12ac6e0298'
